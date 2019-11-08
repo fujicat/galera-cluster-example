722ccb9916e9bcd4c56e391e00d7e897ba0f5dac
片桐孝昌 <t.katagiri1973@gmail.com>
2019-11-08 16:38:48 +0900

    First version.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Galera Cluster verification environment
+
+Docker container for Galera Cluster verification.  
+There are node01 to node05, node01 to node04 have been replicated, and node05 has mysql not started.
+
+Start and stop the service with supervisorctl.
+- Start: `supervisorctl start mysqld`
+- Stop: `supervisorctl stop mysqld`
+
+Each node's mysql is listening on the host computer with port number 11001 to 11005.  
+A database has been created with the name `galera_test` for verification, and you can connect with user name:` galera` and password: `password`.
+
+```
+mysql -u galera -ppassword -P 11001
+```
--- node01/init.sh
+++ node01/init.sh
@@ -8,10 +8,7 @@ do
 done
 
 echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root
-echo "CREATE DATABASE IF NOT EXISTS linkmap;" | /usr/bin/mysql -u root -ppassword
-echo "CREATE DATABASE IF NOT EXISTS linkmap_test;" | /usr/bin/mysql -u root -ppassword
-echo "GRANT ALL ON linkmap.* TO 'linkmap'@'localhost' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
-echo "GRANT ALL ON linkmap.* TO 'linkmap'@'%' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
-echo "GRANT ALL ON linkmap_test.* TO 'linkmap'@'localhost' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
-echo "GRANT ALL ON linkmap_test.* TO 'linkmap'@'%' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
+echo "CREATE DATABASE IF NOT EXISTS galera_test;" | /usr/bin/mysql -u root -ppassword
+echo "GRANT ALL ON galera_test.* TO 'galera'@'localhost' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
+echo "GRANT ALL ON galera_test.* TO 'galera'@'%' IDENTIFIED BY 'password';" | /usr/bin/mysql -u root -ppassword
 
